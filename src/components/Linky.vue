33a42d8c9abb1b1fd38c43c7267d352e18f169e7
taylor <rhodes4ao@gmail.com>
2020-03-15 11:34:31 -0700

    css ?

--- src/components/Linky.vue
+++ src/components/Linky.vue
@@ -42,7 +42,9 @@ export default {
 
 </script>
 
-<style lang='stylus' scoped>
+
+<style lang='stylus'>
+// adding scoped causes ui bugs in context rows I do not understand how (ghosts)
 
 .centered
     text-align: center
